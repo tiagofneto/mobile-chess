88e53e53860f98a037f0d6f7dd64b9bb59481f17
Tiago Neto <tiago.f.neto@gmail.com>
2020-08-04 10:59:41 +0100

    All moves working, game playable

--- lib/board.dart
+++ lib/board.dart
@@ -5,8 +5,10 @@ import 'package:chess/tile.dart';
 class Board extends StatefulWidget {
   final Color color1;
   final Color color2;
+  final Color movingColor;
+  //TODO add more colors
 
-  Board(this.color1, this.color2);
+  Board(this.color1, this.color2, this.movingColor);
 
   @override
   _BoardState createState() => _BoardState();
@@ -15,14 +17,16 @@ class Board extends StatefulWidget {
 class _BoardState extends State<Board> {
   List<List<bool>> canMove;
   List<List<Piece>> pieces;
+  //TODO change movignPiece to bool
   Piece movingPiece;
+  String currentPlayer;
 
   @override
   void initState() {
     super.initState();
-    canMove = List.generate(8, (index) => List.generate(8, (index) => false));
-    movingPiece = null;
+    _clearMoves();
     _initialBoard();
+    currentPlayer = "white";
   }
 
   void _initialBoard() {
@@ -32,59 +36,72 @@ class _BoardState extends State<Board> {
         row <= 1 ? pieceColor = "black" : pieceColor = "white";
         Piece piece;
         if (row == 0 || row == 7) {
-          piece = _fabricatePiece(col, row, pieceColor);
+          piece = Piece(_initPieceName(col), pieceColor, Position(col, row));
         } else if (row == 1 || row == 6) {
-          piece = Pawn(pieceColor, Position(col, row));
+          piece = Piece("pawn", pieceColor, Position(col, row));
         }
         return piece;
       });
     });
   }
 
-  Piece _fabricatePiece(int col, int row, String pieceColor) {
-    Position pos = Position(col, row);
+  String _initPieceName(int col) {
+    //TODO create env variables
     switch (col) {
       case 0:
-        return Rook(pieceColor, pos);
+        return "rook";
       case 1:
-        return Knight(pieceColor, pos);
+        return "knight";
       case 2:
-        return Bishop(pieceColor, pos);
+        return "bishop";
       case 3:
-        return Queen(pieceColor, pos);
+        return "queen";
       case 4:
-        return King(pieceColor, pos);
+        return "king";
       case 5:
-        return Bishop(pieceColor, pos);
+        return "bishop";
       case 6:
-        return Knight(pieceColor, pos);
+        return "knight";
       case 7:
-        return Rook(pieceColor, pos);
+        return "rook";
       default:
         return null;
     }
   }
 
+  //TODO optimize
   void _onTileClicked(Piece piece, Position tilePos) {
-    if (movingPiece == null) {
-      setState(() {
-        canMove =
-            List.generate(8, (index) => List.generate(8, (index) => false));
-      });
-      if (piece != null) {
+    if (movingPiece != null) {
+      if (canMove[tilePos.row][tilePos.col]) {
+        //FIXME lowercase
+        pieces[tilePos.row][tilePos.col] = Piece(movingPiece.name.toLowerCase(),
+            movingPiece.color, Position(tilePos.col, tilePos.row));
+
+        pieces[movingPiece.pos.row][movingPiece.pos.col] = null;
+        _swapPlayers();
+      }
+      setState(_clearMoves);
+    } else {
+      setState(_clearMoves);
+      if (piece != null && piece.color == currentPlayer) {
         movingPiece = piece;
         //Getting valid move positions
-        for (List<Position> direction in piece.listMoves()) {
-          print("SWITCH");
+        List<List<Position>> moves = piece.listMoves();
+        for (List<Position> direction in moves) {
           for (Position pos in direction) {
-            print(
-                "Row: " + pos.row.toString() + " | Col: " + pos.col.toString());
-            // setState(() {
-            //   if (pieces[pos.row][pos.col].color == )
-            //   canMove[pos.row][pos.col] = true;
-            // });
             Piece currPiece = pieces[pos.row][pos.col];
             if (currPiece != null && currPiece.color == piece.color) break;
+
+            //FIXME lowercase
+            if (piece.name.toLowerCase() == "pawn") {
+              int index = moves.indexOf(direction);
+              //Can only move diagonal to eat
+              if (currPiece == null) {
+                if (index == 0 || index == 2) break;
+              } else {
+                if (index == 1) break;
+              }
+            }
 
             setState(() {
               canMove[pos.row][pos.col] = true;
@@ -94,8 +111,19 @@ class _BoardState extends State<Board> {
           }
         }
       }
+    }
+  }
+
+  void _clearMoves() {
+    movingPiece = null;
+    canMove = List.generate(8, (index) => List.generate(8, (index) => false));
+  }
+
+  void _swapPlayers() {
+    if (currentPlayer == "white") {
+      currentPlayer = "black";
     } else {
-      //TODO
+      currentPlayer = "white";
     }
   }
 
@@ -124,6 +152,9 @@ class _BoardState extends State<Board> {
   }
 
   Color _calculateTileColor(int col, int row) {
+    if (movingPiece != null) if (movingPiece.pos.col == col &&
+        movingPiece.pos.row == row) return widget.movingColor;
+
     if ((row.isEven && col.isEven) || (row.isOdd && col.isOdd))
       return widget.color1;
     else
--- lib/tile.dart
+++ lib/tile.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:chess/piece.dart';
 
@@ -35,13 +37,23 @@ class Position {
 
   Position(this.col, this.row) {
     if (col < 0 || col > 7 || row < 0 || row > 7) {
-      throw PositionOutOfBoundsException();
+      throw "Out of bounds!";
     }
   }
 
-//TODO return iterator or list
+  static List<Position> getMove(int currentCol, int currentRow) {
+    try {
+      return [Position(currentCol, currentRow)];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  //TODO return iterator or list
   List<List<Position>> getStraight() {
     List<List<Position>> moves = List<List<Position>>(4);
+
+    //Comments considering white pieces
     //UP
     moves[0] = List.generate(row, (index) => Position(col, row - index - 1));
     //DOWN
@@ -55,9 +67,56 @@ class Position {
 
     return moves;
   }
-}
 
-//TODO check if necessary
-class PositionOutOfBoundsException implements Exception {
-  String errMsg() => 'Position out of bounds!';
+  List<List<Position>> getDiagonal() {
+    List<List<Position>> moves = List<List<Position>>(4);
+
+    //Comments considering white pieces
+    //TOP LEFT
+    moves[0] = List.generate(
+        min(col, row), (index) => Position(col - index - 1, row - index - 1));
+    //TOP RIGHT
+    moves[1] = List.generate(min(7 - col, row),
+        (index) => Position(col + index + 1, row - index - 1));
+    //BOTTOM LEFT
+    moves[2] = List.generate(min(col, 7 - row),
+        (index) => Position(col - index - 1, row + index + 1));
+    //BOTTOM LEFT
+    moves[3] = List.generate(min(7 - col, 7 - row),
+        (index) => Position(col + index + 1, row + index + 1));
+
+    return moves;
+  }
+
+  //Considering white
+  List<List<Position>> getFront() {
+    List<List<Position>> moves = List<List<Position>>(3);
+
+    //TOP LEFT
+    moves[0] = getMove(col - 1, row - 1);
+
+    //TOP
+    moves[1] = getMove(col, row - 1);
+
+    //TOP RIGHT
+    moves[2] = getMove(col + 1, row - 1);
+
+    return moves;
+  }
+
+  //Considering white
+  List<List<Position>> getBack() {
+    List<List<Position>> moves = List<List<Position>>(3);
+
+    //TOP LEFT
+    moves[0] = getMove(col - 1, row + 1);
+
+    //TOP
+    moves[1] = getMove(col, row + 1);
+
+    //TOP RIGHT
+    moves[2] = getMove(col + 1, row + 1);
+
+    return moves;
+  }
 }
